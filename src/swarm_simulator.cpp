c0d0123624c1f7a6a4e3ed337b13ee0fd27b399c
Kumar Krishna Agarwal <kumar.1994.14@gmail.com>
2015-02-01 23:20:25 +0530

    Added Custom Message for publishing obstacle data in topic /obstacleList

--- src/swarm_simulator.cpp
+++ src/swarm_simulator.cpp
@@ -4,19 +4,16 @@
 #include "std_msgs/String.h"
 #include "gazebo_msgs/ModelState.h"
 #include <gazebo_msgs/GetModelState.h>
-
 #include <gazebo_msgs/GetWorldProperties.h>
 #include <stdio.h>
 #include <string.h>
 
+#include "swarm_simulator/obstacleList.h"
+#include "swarm_simulator/obstacleData.h"
+
 #define print(a) std::cout << a << std::endl;
 
-typedef struct param{
-  double x;
-  double y;
-  double radius;
-  int shape;
-}param;
+using namespace std;
 
 int main(int argc, char **argv)
 {
@@ -24,13 +21,12 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "CreateController");
 
   ros::NodeHandle n;
-
+  ros::Publisher pub = n.advertise<swarm_simulator::obstacleList>("/obstacleList", 1);
   ros::ServiceClient client = n.serviceClient<gazebo_msgs::GetWorldProperties>("/gazebo/get_world_properties"); 
   gazebo_msgs::GetWorldProperties prop;
   std::vector<std::string> s;
   if(client.call(prop))
     s=prop.response.model_names;
-    //  ROS_INFO("List :%d", (prop.response.model_names.size()));
 
   ros::ServiceClient serv_client = n.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state"); 
   gazebo_msgs::GetModelState getmodelstate;
@@ -38,7 +34,6 @@ int main(int argc, char **argv)
                   
   ros::Rate loop_rate(5);
   int **img;
- // std::cout << "here";
   img=(int **)malloc(sizeof(int *)*2400);
   for(int i=0;i<2400;i++){
     img[i] =(int *)malloc(sizeof(int)*2400);
@@ -52,34 +47,36 @@ int main(int argc, char **argv)
 
   while (ros::ok())
   {
-
-   i=0;
-   std::vector<param> obs;
+    i=0;
+    swarm_simulator::obstacleList msg;
     while(i < s_l){
-      std::cout << s[i] << std::endl;
       getmodelstate.request.model_name=s[i];
-    
+
       if(serv_client.call(getmodelstate)){
-      //  std::cout << getmodelstate.response.pose << std::endl;
-        param toAdd;
+
+        swarm_simulator::obstacleData toAdd;
         toAdd.x=getmodelstate.response.pose.position.x;
         toAdd.y=getmodelstate.response.pose.position.y;
-        toAdd.radius = 2.0;
-        if(s[i].find("cylinder")==-1){
+        toAdd.radius = 2.0; 
+
+        if(s[i].find("cylinder")!=std::string::npos){
           toAdd.shape=0;
-          obs.push_back(toAdd);
+          msg.obstacles.push_back(toAdd);
         }
-        else if(s[i].find("box")==-1){
+        else if(s[i].find("box")!=std::string::npos){
           toAdd.shape=1;
-          obs.push_back(toAdd);
+          msg.obstacles.push_back(toAdd);
         }
-        else if(s[i].find("sphere")==-1){
+        else if(s[i].find("sphere")!=std::string::npos){
           toAdd.shape=2;
-          obs.push_back(toAdd);
+          msg.obstacles.push_back(toAdd);
         }
       }
       i++;
-    }    
+    }   
+    pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
   }
   return 0;
 } 
