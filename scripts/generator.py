1a297d9148bcad101890f3bf8ff65bd083ce1449
Kumar Krishna Agarwal <kumar.1994.14@gmail.com>
2015-03-16 22:09:27 +0530

    Updated for n agents

---
+++ scripts/generator.py
@@ -0,0 +1,45 @@
+import sys
+
+
+''' This is the generator python script for generating the .launch file for n-agents.
+    The script needs to be improved to accomomodate additions at a later point.
+    
+    TODO:
+        Port to xml tree parser with some library like lxml for python.
+        
+'''
+def main( n):
+    print "<launch>"
+    print """ <include file="$(find gazebo_ros)/launch/empty_world.launch">     
+    <arg name="world_name" value="$(find swarm_simulator)/world/arena.world"/>      
+  </include>  
+
+ """
+    for i in range(0,n):
+    	newbot=""
+        botname="swarmbot"
+        botname+=str(i) 
+        newbot+="""<group ns="""
+        newbot+="\"" + str(botname) + "\""
+        newbot+=""">\n"""
+        newbot+="""<include file="$(find swarm_simulator)/launch/one_swarmRobot.launch">
+"""
+        botID=i
+        newbot+="""<arg name="robot_name" value="""
+        newbot+="\"" + str(botname) + "\""
+        newbot+="""/>\n"""
+        newbot+="""<arg name="init_pose" value="""
+        x = -12+0.5*i
+        y = -12+0.5*i
+        newbot+="\"" + "-x " + str(x) + " -y " + str(y) + " -z 0" +"\"" + "/>\n"
+        newbot+="</include>\n"
+        newbot+="""</group>\n"""
+    	print newbot
+    print """ 
+        <group ns="swarm_node">
+        <node pkg="swarm_simulator" type="swarm_simulator_node" name="swarm_simulator_node" respawn="false" />
+        </group>"""
+    print "</launch>"
+
+if __name__ == '__main__':
+	main(int(sys.argv[1]))
---
+++ scripts/generator.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+n=$1
+python scripts/generator.py $n > launch/swarm.launch
+
