f54f21a858e33efc8093cbee1b104357c8b0f023
dementor <soumyadeep9@gmail.com>
2015-07-15 13:06:18 +0200

    Refactored code to make it real time

--- src/swarm_simulator.cpp
+++ src/swarm_simulator.cpp
@@ -1,13 +1,11 @@
 #include "ros/ros.h"
-#include <math.h>
-#include "geometry_msgs/Twist.h"
-#include "std_msgs/String.h"
-#include "gazebo_msgs/ModelState.h"
-#include <gazebo_msgs/GetModelState.h>
-#include <gazebo_msgs/GetWorldProperties.h>
-#include <stdio.h>
-#include <string.h>
 
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Pose.h"
+#include "std_msgs/String.h"
+#include "gazebo_msgs/ModelStates.h"
+#include <string>
+#include <vector>
 #include "swarm_simulator/obstacleList.h"
 #include "swarm_simulator/obstacleData.h"
 
@@ -15,73 +13,66 @@
 
 using namespace std;
 
+std::vector<std::string> s;
+std::vector<geometry_msgs::Pose> poses;
+
+void ModelStateCallback(const gazebo_msgs::ModelStates::ConstPtr& msg)
+{
+	s = msg->name;
+	poses = msg->pose;
+}
+
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "CreateController");
+	ros::init(argc, argv, "swarm_simulator_node");
 
-  ros::NodeHandle n;
-  ros::Publisher pub = n.advertise<swarm_simulator::obstacleList>("/obstacleList", 1);
-  ros::ServiceClient client = n.serviceClient<gazebo_msgs::GetWorldProperties>("/gazebo/get_world_properties"); 
-  gazebo_msgs::GetWorldProperties prop;
-  std::vector<std::string> s;
-  if(client.call(prop))
-    s=prop.response.model_names;
-
-  ros::ServiceClient serv_client = n.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state"); 
-  gazebo_msgs::GetModelState getmodelstate;
-  gazebo_msgs::ModelState modelstate;
+	ros::NodeHandle n;
+	ros::Publisher pub = n.advertise<swarm_simulator::obstacleList>("/obstacleList", 1);
+	ros::Subscriber sub = n.subscribe<gazebo_msgs::ModelStates>("/gazebo/model_states",10,ModelStateCallback);
                   
-  ros::Rate loop_rate(5);
-  int **img;
-  img=(int **)malloc(sizeof(int *)*2400);
-  for(int i=0;i<2400;i++){
-    img[i] =(int *)malloc(sizeof(int)*2400);
-    for(int j=0;j<2400;j++)
-      img[i][j]=0;
-  }
+	ros::Rate loop_rate(5);
 
-  int count = 0;
-  int s_l=s.size();
-  int i=0;
 
-  while (ros::ok())
-  {
-    i=0;
-    swarm_simulator::obstacleList msg;
-    while(i < s_l){
-      getmodelstate.request.model_name=s[i];
+	while (ros::ok())
+	{
+		swarm_simulator::obstacleList msg;
+		
+		for(unsigned i=0;i<s.size();i++)
+		{
+			swarm_simulator::obstacleData toAdd;
+			toAdd.x = poses[i].position.x;
+			toAdd.y = poses[i].position.y;
 
-      if(serv_client.call(getmodelstate)){
-
-        swarm_simulator::obstacleData toAdd;
-        toAdd.x=getmodelstate.response.pose.position.x;
-        toAdd.y=getmodelstate.response.pose.position.y;
-        toAdd.radius = 2.0; 
-
-        if(s[i].find("cylinder")!=std::string::npos){
-          toAdd.shape=0;
-          msg.obstacles.push_back(toAdd);
-        }
-        else if(s[i].find("box")!=std::string::npos){
-          toAdd.shape=1;
-          msg.obstacles.push_back(toAdd);
-        }
-        else if(s[i].find("sphere")!=std::string::npos){
-          toAdd.shape=2;
-          msg.obstacles.push_back(toAdd);
-        }
-        else if(s[i].find("swarmbot")!=std::string::npos){
-          toAdd.shape = s[i][s[i].size()-1]-'0';
-          toAdd.radius = 1.5;
-          msg.obstacles.push_back(toAdd);
-        }
-      }
-      i++;
-    }   
-    pub.publish(msg);
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
-  return 0;
+			if(s[i].find("cylinder")!=std::string::npos)
+			{
+				toAdd.shape=0;
+		    	toAdd.radius = 2;
+				msg.obstacles.push_back(toAdd);
+			}
+			else if(s[i].find("box")!=std::string::npos)
+			{
+				toAdd.shape=1;
+		    	toAdd.radius = 2;
+				msg.obstacles.push_back(toAdd);
+			}
+			else if(s[i].find("sphere")!=std::string::npos)
+			{
+				toAdd.shape=2;
+				toAdd.radius = 2;
+				msg.obstacles.push_back(toAdd);
+			}
+			else if(s[i].find("swarmbot")!=std::string::npos)
+			{
+				toAdd.shape = s[i][s[i].size()-1]-'0';
+				toAdd.radius = 1.5;
+				msg.obstacles.push_back(toAdd);
+			}			
+		}
+		
+		pub.publish(msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+	return 0;
 } 
